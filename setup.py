083cd9128b3cfa610bd10eb668719de3cc6336d4
Misael Galeano Gemade <misael.galeano@est.iudigital.edu.co>
2025-02-23 23:48:26 +0000

    Carga inicial

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Andres Callejas",
+    author_email="",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "matplotlib"
+    ]
+    
+    
+)
